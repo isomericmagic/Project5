2086fa81c2f874dc8b5909b1a0308f6ff2fd5774
isomericmagic <brian@isomericmagic.com>
2017-12-12 14:31:15 -0800

    removed images
    old jQuery plugin / no longer needed

--- employee_directory_v1/js/employeeDirectory.js
+++ employee_directory_v1/js/employeeDirectory.js
@@ -1,40 +1,37 @@
 $(document).ready(function () {
+  function toTitleCase(str)
+  {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
   $.getJSON("https://randomuser.me/api/?results=12", function getEmployees (data) {
     var employeeNumber = 0;
     $.each( data.results, function (i, employee) {
+      var employeeAddress = employee.location.street + employee.location.city + ", " + employee.location.state + ' ' + employee.location.postcode;
+      employeeAddress = toTitleCase(employeeAddress);
+      employeeCity = toTitleCase(employee.location.city);
+      employeeName = toTitleCase(employee.name.first + ' ' + employee.name.last);
+      employeeBirthday = employee.dob.slice(0,10);
       var employeeHTML = '<div class="box" data-featherlight="#mylightbox' + employeeNumber.toString() + '">';
       employeeHTML += '<div class="photo" style="background-image: url(\'' + employee.picture.large + '\');"></div>';
       employeeHTML += '<div class="student-info">';
-      employeeHTML += '<p class="name">'+ employee.name.first + ' ' + employee.name.last + '</p>';
+      employeeHTML += '<p class="name">'+ employeeName + '</p>';
       employeeHTML += '<p class="email">'+ employee.email +'</p>';
-      employeeHTML += '<p class="city">'+ employee.location.city +'</p>';
+      employeeHTML += '<p class="city">'+ employeeCity +'</p>';
       employeeHTML += '</div></div>';
       employeeHTML += '<div hidden class="pop-up-box" id="mylightbox' + employeeNumber.toString() + '">';
       employeeHTML += '<div class="pop-up-photo" style="background-image: url(\'' + employee.picture.large + '\');"></div>';
       employeeHTML += '<div class="pop-up-student-info">';
       employeeHTML += '<br>';
-      employeeHTML += '<p class="pop-up-name">'+ employee.name.first + ' ' + employee.name.last + '</p>';
+      employeeHTML += '<p class="pop-up-name">'+ employeeName + '</p>';
       employeeHTML += '<p class="pop-up-email">'+ employee.email +'</p>';
-      employeeHTML += '<p class="pop-up-city">'+ employee.location.city +'</p>';
+      employeeHTML += '<p class="pop-up-city">'+ employeeCity +'</p>';
       employeeHTML += '<p class="pop-up-line">_________________________________</p>';
-      employeeHTML += '<p class="pop-up-details">(310) 257-3006</p>';
-      employeeHTML += '<p class="pop-up-details">1315 Storm Parkway Torrance, CA 90501</p>';
-      employeeHTML += '<p class="pop-up-details">01/01/2015</p>';
+      employeeHTML += '<p class="pop-up-details">'+ employee.phone +'</p>';
+      employeeHTML += '<p class="pop-up-details">'+ employeeAddress +'</p>';
+      employeeHTML += '<p class="pop-up-details">'+ employeeBirthday +'</p>';
       employeeHTML += '</div></div>';
       $('#primary-container').append(employeeHTML);
       employeeNumber ++;
     });
   });
-}); // end ready
-
-
-/* stuff to grab:
-
-data.results[0].name.first
-data.results[0].name.last
-data.results[0].email
-data.results[0].location.city
-data.results[0].picture.large
-
-
-*/
+});
