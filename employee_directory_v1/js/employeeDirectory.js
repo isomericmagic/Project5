e483716377442a486972163bc6e125cf1bf68933
isomericmagic <brian@isomericmagic.com>
2017-12-12 13:53:19 -0800

    initial working project
    needs some minor touching up but other than that this seems to be working.

--- employee_directory_v1/js/employeeDirectory.js
+++ employee_directory_v1/js/employeeDirectory.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
   $.getJSON("https://randomuser.me/api/?results=12", function getEmployees (data) {
-    //console.log(data.results);
+    var employeeNumber = 0;
     $.each( data.results, function (i, employee) {
-      var employeeHTML = '<a class="box" href=\'' + employee.picture.large + '\' rel="lightbox"';
+      var employeeHTML = '<div class="box" data-featherlight="#mylightbox' + employeeNumber.toString() + '">';
       employeeHTML += '<div class="photo" style="background-image: url(\'' + employee.picture.large + '\');"></div>';
       employeeHTML += '<div class="student-info">';
       employeeHTML += '<p class="name">'+ employee.name.first + ' ' + employee.name.last + '</p>';
       employeeHTML += '<p class="email">'+ employee.email +'</p>';
       employeeHTML += '<p class="city">'+ employee.location.city +'</p>';
-      employeeHTML += '</div></a>';
+      employeeHTML += '</div></div>';
+      employeeHTML += '<div hidden class="pop-up-box" id="mylightbox' + employeeNumber.toString() + '">';
+      employeeHTML += '<div class="pop-up-photo" style="background-image: url(\'' + employee.picture.large + '\');"></div>';
+      employeeHTML += '<div class="pop-up-student-info">';
+      employeeHTML += '<br>';
+      employeeHTML += '<p class="pop-up-name">'+ employee.name.first + ' ' + employee.name.last + '</p>';
+      employeeHTML += '<p class="pop-up-email">'+ employee.email +'</p>';
+      employeeHTML += '<p class="pop-up-city">'+ employee.location.city +'</p>';
+      employeeHTML += '<p class="pop-up-line">_________________________________</p>';
+      employeeHTML += '<p class="pop-up-details">(310) 257-3006</p>';
+      employeeHTML += '<p class="pop-up-details">1315 Storm Parkway Torrance, CA 90501</p>';
+      employeeHTML += '<p class="pop-up-details">01/01/2015</p>';
+      employeeHTML += '</div></div>';
       $('#primary-container').append(employeeHTML);
+      employeeNumber ++;
     });
   });
 }); // end ready
